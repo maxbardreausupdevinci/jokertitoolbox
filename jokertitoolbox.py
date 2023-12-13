21ba4bd8ba75e1b020522acaa1ca66808f057b26
Maxime Bardreau <146346038+maxbardreausupdevinci@users.noreply.github.com>
2023-12-13 15:11:20 +0100

    début

---
+++ jokertitoolbox.py
@@ -0,0 +1,14 @@
+#Ce script permet de faire un test d'intrusion sur une machine distante, il prend comme argument l'adresse IP de la machine distante, et appele les scripts bruteforcesshmdp.py, nmap.py et les exploit compatible afin de rédiger un rapport de test d'intrusion dans un fichier texte.
+
+#vérification de toutes les dépendances et installation si nécessaire
+
+#appel du script nmap.py
+
+#appel du script bruteforcesshmdp.py
+
+#appel des exploits compatibles
+
+#rédaction du rapport de test d'intrusion dans un fichier texte
+
+
+
